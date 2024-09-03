a5173dd6ff08449d87a3abd16e10963841bd4dfe
Ahmednoshey <ahmedeldomiaty0@gmail.com>
2024-09-03 16:40:45 +0300

    React-onlinestore

--- Front End/src/pages/Home/Home.jsx
+++ Front End/src/pages/Home/Home.jsx
@@ -1,84 +1,97 @@
-import React, { useEffect, useState } from "react";
-import "./Home.css";
-import { Box, Paper, Typography, IconButton } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-const Home = () => {
+// import React, { useEffect, useState } from "react";
+// import "./Home.css";
+// import { Box, Paper, Typography, IconButton } from "@mui/material";
+// import CloseIcon from "@mui/icons-material/Close";
+// const Home = () => {
 
-  const [mydata, setmydata] = useState([]);
-  useEffect(() => {
-  fetch("http://localhost:3100/mydata")
-  .then((response) => response.json())
-  .then((data) => setmydata(data));
-  }, []);
+//   const [mydata, setmydata] = useState([]);
+//   // useEffect(() => {
+//   // fetch("http://localhost:3100/mydata")
+//   // .then((response) => response.json())
+//   // .then((data) => setmydata(data));
+//   // }, []);
 
-let totalprice = 0;
+// let totalprice = 0;
 
-  return (
-    <Box>
+//   return (
+//     <Box>
   
-        {mydata.map((item) => {
+//         {mydata.map((item) => {
 
-totalprice += item.price
+// totalprice += item.price
 
-return (
+// return (
 
-<Paper
-key={item.title}
-sx={{
-  width: "366px",
-  display: "flex",
-  justifyContent: "space-between",
-  pt: "27px",
-  pb: "7px",
-  mt: "22px",
-  position: "relative",
-}}
->
+// <Paper
+// key={item.title}
+// sx={{
+//   width: "366px",
+//   display: "flex",
+//   justifyContent: "space-between",
+//   pt: "27px",
+//   pb: "7px",
+//   mt: "22px",
+//   position: "relative",
+// }}
+// >
             
-          <Typography sx={{ ml: "16px" }} variant="h6">
-          {item.title}
-        </Typography>
-        <Typography
-          sx={{
-            mr: "33px",
-            fontWeight: "500",
-            fontSize: "1.2em",
-            opacity: "0.8",
-          }}
-          variant="h6"
-        >
-          ${item.price}
-        </Typography>
+//           <Typography sx={{ ml: "16px" }} variant="h6">
+//           {item.title}
+//         </Typography>
+//         <Typography
+//           sx={{
+//             mr: "33px",
+//             fontWeight: "500",
+//             fontSize: "1.2em",
+//             opacity: "0.8",
+//           }}
+//           variant="h6"
+//         >
+//           ${item.price}
+//         </Typography>
 
-        <IconButton
-          sx={{
-            position: "absolute",
-            right: "0",
-            top: "0",
-          }}
-          onClick={() => {
-            fetch(`http://localhost:3100/mydata/${item.id}`,{
-              method: "DELETE"
-            })
-            const newdata = mydata.filter((object) => {
-              return object.id !== item.id
-            })
-            setmydata(newdata)
-          }}>
-          <CloseIcon sx={{ fontSize: "20px" }} />
-        </IconButton>
-      </Paper>
-);
+//         <IconButton
+//           sx={{
+//             position: "absolute",
+//             right: "0",
+//             top: "0",
+//           }}
+//           onClick={() => {
+//             fetch(`http://localhost:3100/mydata/${item.id}`,{
+//               method: "DELETE"
+//             })
+//             const newdata = mydata.filter((object) => {
+//               return object.id !== item.id
+//             })
+//             setmydata(newdata)
+//           }}>
+//           <CloseIcon sx={{ fontSize: "20px" }} />
+//         </IconButton>
+//       </Paper>
+// );
 
-})}
+// })}
 
       
-      <Typography textAlign="center" mt="50px" variant="h6">&#128073; You Spend ${totalprice}</Typography>
+//       <Typography textAlign="center" mt="50px" variant="h6">&#128073; You Spend ${totalprice}</Typography>
 
       
     
-    </Box>
+//     </Box>
+//   );
+// };
+
+// export default Home;
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
   );
-};
+}
 
 export default Home;
+
--- Front End/src/pages/create/Create.jsx
+++ Front End/src/pages/create/Create.jsx
@@ -1,83 +1,95 @@
-import "./Create.css";
-import { Box, Button, InputAdornment, TextField } from "@mui/material";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
+// import "./Create.css";
+// import { Box, Button, InputAdornment, TextField } from "@mui/material";
+// import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+// import { useNavigate } from "react-router-dom";
+// import { useForm } from "react-hook-form";
+
+// const Create = () => {
+//   const navigate = useNavigate();
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm();
+
+//   const onSubmit = ({ title, price }) => {
+//     price = Number(price)
+//     fetch("http://localhost:3100/mydata", {
+//       method: "post",
+//       headers: {
+//         "content-type": "aplication/json",
+//       },
+//       body: JSON.stringify({ title, price }),
+//     }).then(() => {
+//       navigate("/");
+//     });
+//   };
+
+//   return (
+//     <Box
+//       onSubmit={handleSubmit(onSubmit)}
+//       component="form"
+//       sx={{ width: "380px" }}
+//     >
+//       <TextField
+//         {...register("title", {
+//           required: { value: true, message: "This TextField is Required" },
+//           minLength: {
+//             value: 3,
+//             message: "You Should Write 3 Letters Or More",
+//           },
+//         })}
+//         fullWidth
+//         label="Transection Title"
+//         sx={{ display: "block", mt: "22px" }}
+//         InputProps={{
+//           startAdornment: (
+//             <InputAdornment position="start">&#128073;</InputAdornment>
+//           ),
+//         }}
+//         variant="filled"
+//         error={Boolean(errors.title)}
+//         helperText={
+//           Boolean(errors.title) ? errors.title?.message.toString() : null
+//         }
+//       />
+
+//       <TextField
+//         {...register("price", {
+//           required: { value: true, message: "This TextField is Required" },
+//         })}
+//         fullWidth
+//         label="Amount"
+//         sx={{ display: "block", mt: "22px" }}
+//         InputProps={{
+//           startAdornment: <InputAdornment position="start">$</InputAdornment>,
+//         }}
+//         variant="filled"
+//         error={Boolean(errors.price)}
+//         type="number"
+//         helperText={
+//           Boolean(errors.price) ? errors.price?.message.toString() : null
+//         }
+//       />
+
+//       <Button type="submit" color="primary" variant="contained" sx={{ mt: "20px" }}>
+//         Submit
+//         <KeyboardArrowRightIcon />
+//       </Button>
+//     </Box>
+//   );
+// };
+
+// export default Create;
+
+import React from 'react';
 
 const Create = () => {
-  const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = ({ title, price }) => {
-    price = Number(price)
-    fetch("http://localhost:3100/mydata", {
-      method: "post",
-      headers: {
-        "content-type": "aplication/json",
-      },
-      body: JSON.stringify({ title, price }),
-    }).then(() => {
-      navigate("/");
-    });
-  };
-
   return (
-    <Box
-      onSubmit={handleSubmit(onSubmit)}
-      component="form"
-      sx={{ width: "380px" }}
-    >
-      <TextField
-        {...register("title", {
-          required: { value: true, message: "This TextField is Required" },
-          minLength: {
-            value: 3,
-            message: "You Should Write 3 Letters Or More",
-          },
-        })}
-        fullWidth
-        label="Transection Title"
-        sx={{ display: "block", mt: "22px" }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">&#128073;</InputAdornment>
-          ),
-        }}
-        variant="filled"
-        error={Boolean(errors.title)}
-        helperText={
-          Boolean(errors.title) ? errors.title?.message.toString() : null
-        }
-      />
-
-      <TextField
-        {...register("price", {
-          required: { value: true, message: "This TextField is Required" },
-        })}
-        fullWidth
-        label="Amount"
-        sx={{ display: "block", mt: "22px" }}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-        }}
-        variant="filled"
-        error={Boolean(errors.price)}
-        type="number"
-        helperText={
-          Boolean(errors.price) ? errors.price?.message.toString() : null
-        }
-      />
-
-      <Button type="submit" color="primary" variant="contained" sx={{ mt: "20px" }}>
-        Submit
-        <KeyboardArrowRightIcon />
-      </Button>
-    </Box>
+    <div>
+      
+    </div>
   );
-};
+}
 
 export default Create;
